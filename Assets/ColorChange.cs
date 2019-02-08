a23014246503f980340720b6fbbe24c55cdf9c49
Walaja, SaiBhargavi <SaiBhargavi.Walaja@cdk.com>
2019-02-08 12:54:46 +0530

    second commi t

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+    public GameObject[] bodycolorred;
+    public GameObject[] bodycolorblack;
+    public GameObject[] glasscolor;
+    public GameObject[] steel;
+    private int count = 0;
+    public Texture Button;
+    public Texture Button1;
+    public Texture Button2;
+    public Texture[] textures = new Texture[3];
+
+    void Start()
+    {
+        for (count = 0; count < bodycolorred.Length; count++)
+        {
+            bodycolorred[count].GetComponent<Renderer>().material.mainTexture = textures[0];
+            bodycolorred[count].GetComponent<Renderer>().material.color = Color.red;
+        }
+        count = 0;
+        for (count = 0; count < bodycolorblack.Length; count++)
+        {
+            bodycolorblack[count].GetComponent<Renderer>().material.color = Color.black;
+        }
+        count = 0;
+        for (count = 0; count < glasscolor.Length; count++)
+        {
+            glasscolor[count].GetComponent<Renderer>().material.color = Color.clear;
+        }
+        count = 0;
+        for (count = 0; count < steel.Length; count++)
+        {
+            steel[count].GetComponent<Renderer>().material.color = Color.grey;
+        }
+    }
+
+
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(40, 40, 100, 100), Button))
+        {
+            for (count = 0; count < bodycolorred.Length; count++)
+            {
+                bodycolorred[count].GetComponent<Renderer>().material.mainTexture = textures[0];
+                bodycolorred[count].GetComponent<Renderer>().material.color = Color.red;
+            }
+            count = 0;
+            for (count = 0; count < bodycolorblack.Length; count++)
+            {
+                bodycolorblack[count].GetComponent<Renderer>().material.color = Color.black;
+            }
+            count = 0;
+            for (count = 0; count < glasscolor.Length; count++)
+            {
+                glasscolor[count].GetComponent<Renderer>().material.color = Color.clear;
+            }
+            count = 0;
+            for (count = 0; count < steel.Length; count++)
+            {
+                steel[count].GetComponent<Renderer>().material.color = Color.grey;
+            }
+        }
+        if (GUI.Button(new Rect(40, 200, 100, 100), Button1))
+        {
+            for (count = 0; count < bodycolorred.Length; count++)
+            {
+                bodycolorred[count].GetComponent<Renderer>().material.mainTexture = textures[1];
+                bodycolorred[count].GetComponent<Renderer>().material.color = Color.black;
+            }
+            count = 0;
+            for (count = 0; count < bodycolorblack.Length; count++)
+            {
+                bodycolorblack[count].GetComponent<Renderer>().material.color = Color.black;
+            }
+            count = 0;
+            for (count = 0; count < glasscolor.Length; count++)
+            {
+                glasscolor[count].GetComponent<Renderer>().material.color = Color.clear;
+            }
+            count = 0;
+            for (count = 0; count < steel.Length; count++)
+            {
+                steel[count].GetComponent<Renderer>().material.color = Color.grey;
+            }
+        }
+        if (GUI.Button(new Rect(40, 360, 100, 100), Button2))
+        {
+            for (count = 0; count < bodycolorred.Length; count++)
+            {
+                bodycolorred[count].GetComponent<Renderer>().material.mainTexture = textures[2];
+                bodycolorred[count].GetComponent<Renderer>().material.color = Color.blue;
+            }
+            count = 0;
+            for (count = 0; count < bodycolorblack.Length; count++)
+            {
+                bodycolorblack[count].GetComponent<Renderer>().material.color = Color.black;
+            }
+            count = 0;
+            for (count = 0; count < glasscolor.Length; count++)
+            {
+                glasscolor[count].GetComponent<Renderer>().material.color = Color.clear;
+            }
+            count = 0;
+            for (count = 0; count < steel.Length; count++)
+            {
+                steel[count].GetComponent<Renderer>().material.color = Color.grey;
+            }
+        }
+    }
+}
---
+++ Assets/rimClick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class rimClick : MonoBehaviour {
+
+    public GameObject replacewheel;
+    public GameObject replacewheel1;
+    public GameObject replacewheel2;
+    public GameObject replacewheel3;
+    public GameObject wheel1;
+    public GameObject wheel2;
+    public GameObject wheel3;
+    public GameObject wheel4;
+    public Texture Button1;
+    public Texture Button2;
+
+    void Start () {
+        replacewheel.SetActive(false);
+        replacewheel1.SetActive(false);
+        replacewheel2.SetActive(false);
+        replacewheel3.SetActive(false);
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(1000, 40, 100, 100), Button1))
+        {
+            wheel1.SetActive(false);
+            wheel2.SetActive(false);
+            wheel3.SetActive(false);
+            wheel4.SetActive(false);
+            replacewheel.SetActive(true);
+            replacewheel1.SetActive(true);
+            replacewheel2.SetActive(true);
+            replacewheel3.SetActive(true);
+        }
+        if (GUI.Button(new Rect(1000, 200, 100, 100), Button2))
+        {
+            replacewheel.SetActive(false);
+            replacewheel1.SetActive(false);
+            replacewheel2.SetActive(false);
+            replacewheel3.SetActive(false);
+            wheel1.SetActive(true);
+            wheel2.SetActive(true);
+            wheel3.SetActive(true);
+            wheel4.SetActive(true);
+        }
+    }
+}
